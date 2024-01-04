832ff6afeebe430a74fd1eef98c634b96208d391
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2024-01-04 22:12:37 +0400

    added code to read csv files

---
+++ main.js
@@ -0,0 +1,49 @@
+const fs = require("fs").promises;
+const fss = require("fs");
+
+// importing the fs with asynchronous functionality
+const directoryPath = "./02_01_2024/entry";
+const Datastore = require("nedb");
+// adding database
+
+const csv = require("csv-parser");
+
+const database = new Datastore("database1.db");
+database.loadDatabase();
+// initilizing the database and calling it
+
+async function main(path) {
+  const data = await readFileNames(path);
+
+  for (element of data) {
+    element = element.replace(".jpg", "");
+    element = element.split(" ");
+    const plate = { PlateCode: element[2], plate: element[3] };
+    database.insert(plate, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+
+  // console.log(data);
+
+  //   getting the names of files and storing it in an array
+}
+
+async function readFileNames(path) {
+  const fileNames = await fs.readdir(path);
+  return fileNames;
+
+  //   using the readdir function from fs to read the names of files and return the data to the  ain function
+}
+
+// main(directoryPath);
+const collectedData = [];
+fss
+  .createReadStream("./data.csv")
+  .pipe(csv({}))
+  .on("data", (data) => collectedData.push(data))
+  .on("end", () => console.log(collectedData));
+
+// console.log(collectedData);
